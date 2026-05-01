1debc44a578664a3eac673cf5375418208422d87
Kevin Ho <kevinkh08@gmail.com>
2026-05-01 16:21:33 -0700

    set up local-argo for dualistic upgrade

--- argocd-initial-install/install.sh
+++ argocd-initial-install/install.sh
@@ -1,6 +1,9 @@
 kubectl create namespace argocd
 kubectl label namespace argocd istio-injection=enabled
 
+kubectl create namespace argocd-prod
+kubectl label namespace argocd-prod istio-injection=enabled
 
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install argocd argo/argo-cd -n argocd --version 9.5.2 -f values.yaml
+helm install argocd argo/argo-cd -n argocd --version 6.9.3 -f values.yaml
+helm install argocd argo/argo-cd -n argocd-prod --version 6.9.3 -f values.yaml
