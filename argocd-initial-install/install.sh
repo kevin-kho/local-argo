29f43fe93d52dbd747e77a8322b3871603739010
Kevin Ho <kevinkh08@gmail.com>
2026-06-08 07:50:45 -0700

    update install.sh to be minimal

--- argocd-initial-install/install.sh
+++ argocd-initial-install/install.sh
@@ -1,9 +1,8 @@
 kubectl create namespace argocd
 kubectl label namespace argocd istio-injection=enabled
+kubectl create namespace topograph
+kubectl label namespace topograph istio-injection=enabled
 
-kubectl create namespace argocd-prod
-kubectl label namespace argocd-prod istio-injection=enabled
 
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install argocd argo/argo-cd -n argocd --version 6.9.3 -f values.yaml
-helm install argocd argo/argo-cd -n argocd-prod --version 6.9.3 -f values-eng.yaml
+helm install argocd argo/argo-cd -n argocd --version 9.5.20 -f values.yaml
